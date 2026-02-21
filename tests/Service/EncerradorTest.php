0e8dbf10bd254b8b5ec726a88b99e92c6841fa3d
martinsllm <lucaslm2010@gmail.com>
2026-02-21 16:04:38 -0300

    Creating mocks

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -7,6 +7,35 @@ use Alura\Leilao\Dao\Leilao as LeilaoDao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
+class LeilaoDaoMock extends LeilaoDao 
+{
+    private $leiloes = [];
+
+    public function recuperarFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+            return $leilao->estaFinalizado();
+        });
+    }
+
+    public function recuperarNaoFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+            return !$leilao->estaFinalizado();
+        });
+    }
+
+    public function salva(Leilao $leilao): void
+    {
+        $this->leiloes[] = $leilao;
+    }
+
+    public function atualiza(Leilao $leilao)
+    {
+        // Não é necessário implementar nada aqui para o teste
+    }
+
+}
 class EncerradorTest extends TestCase
 {
     public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
@@ -14,17 +43,14 @@ class EncerradorTest extends TestCase
         $leilao = new Leilao('Fiat 147 0KM', new \DateTimeImmutable('8 days ago'));
         $leilao2 = new Leilao('Fusca 1972 0KM', new \DateTimeImmutable('10 days ago'));
 
-        $leilaoDao = new LeilaoDao();
+        $leilaoDao = new LeilaoDaoMock();
         $leilaoDao->salva($leilao);
         $leilaoDao->salva($leilao2);
 
-        $encerrador = new Encerrador();
+        $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
-
-        $leiloes = $leilaoDao->recuperarFinalizados();
-
-        self::assertCount(2, $leiloes);
-        self::assertEquals('Fiat 147 0KM', $leiloes[0]->recuperarDescricao());
-        self::assertEquals('Fusca 1972 0KM', $leiloes[1]->recuperarDescricao());
+        
+        $this->assertTrue($leilao->estaFinalizado());
+        $this->assertTrue($leilao2->estaFinalizado());
     }
 }
