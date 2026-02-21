dee2e3e4cecf1b8feb539f0882e5124be4a37a3f
martinsllm <lucaslm2010@gmail.com>
2026-02-21 18:50:22 -0300

    Creating mocks pt.2

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -7,35 +7,6 @@ use Alura\Leilao\Dao\Leilao as LeilaoDao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
-class LeilaoDaoMock extends LeilaoDao 
-{
-    private $leiloes = [];
-
-    public function recuperarFinalizados(): array
-    {
-        return array_filter($this->leiloes, function (Leilao $leilao) {
-            return $leilao->estaFinalizado();
-        });
-    }
-
-    public function recuperarNaoFinalizados(): array
-    {
-        return array_filter($this->leiloes, function (Leilao $leilao) {
-            return !$leilao->estaFinalizado();
-        });
-    }
-
-    public function salva(Leilao $leilao): void
-    {
-        $this->leiloes[] = $leilao;
-    }
-
-    public function atualiza(Leilao $leilao)
-    {
-        // Não é necessário implementar nada aqui para o teste
-    }
-
-}
 class EncerradorTest extends TestCase
 {
     public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
@@ -43,9 +14,14 @@ class EncerradorTest extends TestCase
         $leilao = new Leilao('Fiat 147 0KM', new \DateTimeImmutable('8 days ago'));
         $leilao2 = new Leilao('Fusca 1972 0KM', new \DateTimeImmutable('10 days ago'));
 
-        $leilaoDao = new LeilaoDaoMock();
-        $leilaoDao->salva($leilao);
-        $leilaoDao->salva($leilao2);
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+        $leilaoDao->method('recuperarNaoFinalizados')
+            ->willReturn([$leilao, $leilao2]);
+        $leilaoDao->method('recuperarFinalizados')
+            ->willReturn([$leilao, $leilao2]);
+        $leilaoDao->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withParameterSetsInOrder([$leilao], [$leilao2]);
 
         $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
